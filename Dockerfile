426606fcb1d1c2277fd4a13c212f7a7729a4563c
Drew Blokzyl <dblokzyl@secureworks.com>
2020-08-04 22:05:05 -0400

    streamlining

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,17 @@ FROM kalilinux/kali-rolling:latest
 
 # don't prompt for apt stuffs
 ENV DEBIAN_FRONTEND noninteractive
+ENV LANG en_US.UTF-8
+
+# prep for install
+RUN apt update -y && apt dist-upgrade -y
+
+# build our locale before going through our installs
+RUN apt install -y locales
+RUN echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+RUN locale-gen
 
 # install the tools
-RUN apt-get -y update && apt-get -y dist-upgrade && apt-get -y autoremove && apt-get clean
 RUN apt install -y kali-tools-top10 kali-tools-web kali-tools-passwords man-db exploitdb python3-pexpect python3-bs4 tmux screen zsh neovim python2 awscli python3-boto3 azure-cli python3-azure
 
 # set python3 as default for python
@@ -13,6 +21,13 @@ RUN ln -s /usr/bin/python3 /usr/bin/python
 
 # initialize Metasploit database
 RUN service postgresql start && msfdb init && service postgresql stop
+
+# Grab some sane default configs
+RUN git clone https://github.com/atucom/dotfiles /root/atu-dotfiles
+RUN curl -s https://raw.githubusercontent.com/linuxkd/dotfiles/master/.zshrc_docker --output /root/.zshrc
+RUN curl -s https://gist.githubusercontent.com/linuxkd/9ac20377c4518ba4d76751465391dfa4/raw/61d9487f82810279b6b7f2231b695b42c3a3d2ed/kali-bashrc --output /root/.bashrc
+
+# Copy all of our stuff to the host and expose as a volume
 VOLUME /root /var/lib/postgresql
 
 # expose common ports
@@ -25,5 +40,6 @@ EXPOSE 5555
 EXPOSE 8080
 EXPOSE 8443
 
+# set our common entry point and set init for docker start
 WORKDIR /root
-CMD ["/bin/bash"]
+CMD ["/sbin/init"]
--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,28 @@
+#!/bin/bash
+
+# Deploy Kali as a docker container. Tested on Amazon Linux 2.
+# by Drew Blokzyl (@linuxkd)
+# https://github.com/linuxkd/kali-docker/
+
 sudo yum update -y
-sudo yum install -y docker git
+sudo yum install -y docker jq
 sudo service docker start
 
-sudo docker build -t kali https://raw.githubusercontent.com/linuxkd/kali-docker/master/Dockerfile
+# Create an image named kali from the specified Dockerfile
+sudo docker build -t kali-image https://raw.githubusercontent.com/linuxkd/kali-docker/master/Dockerfile
 
-KALI_ROOT="/pentest/kali-root"
+# Create a container named kali based on our image also named kali
+sudo docker create --name kali kali-image
+sudo docker start kali
 
-sudo mkdir -p $KALI_ROOT
-sudo chmod 777 $KALI_ROOT
-git clone https://github.com/atucom/dotfiles $KALI_ROOT/atu-dotfiles
-curl -s https://raw.githubusercontent.com/linuxkd/dotfiles/master/.zshrc_docker --output $KALI_ROOT/.zshrc
-curl -s https://gist.githubusercontent.com/linuxkd/9ac20377c4518ba4d76751465391dfa4/raw/61d9487f82810279b6b7f2231b695b42c3a3d2ed/kali-bashrc --output $KALI_ROOT/.bashrc
+# The path where we want to easily access our /root folder for the kali container
+sudo mkdir /pentest
 
-# BASH Default
-echo 'sudo alias kali="docker run -p 53:53 -p 53:53/udp -p 80:80 -p 443:443 -p 4444:4444 -p 5555:5555 -p 8080:8080 -p 8443:8443 -e TZ=America/New_York -ti --rm --mount type=bind,src=/pentest/kali-root,dst=/root --mount src=kali-postgres,dst=/var/lib/postgresql kali bash"' | sudo tee -a /etc/skel/.bashrc
-echo 'sudo alias kali-zsh="docker run -p 53:53 -p 53:53/udp -p 80:80 -p 443:443 -p 4444:4444 -p 5555:5555 -p 8080:8080 -p 8443:8443 -e TZ=America/New_York -ti --rm --mount type=bind,src=/pentest/kali-root,dst=/root --mount src=kali-postgres,dst=/var/lib/postgresql kali zsh"' | sudo tee -a /etc/skel/.bashrc
+# Get the local path on disk from the container, then soft link it for easy access
+mount_path="$(sudo docker inspect kali | jq -r '.[].Mounts[0].Source')"
+sudo ln -s "$mount_path" /pentest/kali-root
 
-# ZSH Default
-echo 'alias kali="sudo docker run -p 53:53 -p 53:53/udp -p 80:80 -p 443:443 -p 4444:4444 -p 5555:5555 -p 8080:8080 -p 8443:8443 -e TZ=America/New_York -ti --rm --mount type=bind,src=/pentest/kali-root,dst=/root --mount src=kali-postgres,dst=/var/lib/postgresql kali bash"' | tee -a ~/.bashrc
-echo 'alias kali-zsh="sudo docker run -p 53:53 -p 53:53/udp -p 80:80 -p 443:443 -p 4444:4444 -p 5555:5555 -p 8080:8080 -p 8443:8443 -e TZ=America/New_York -ti --rm --mount type=bind,src=/pentest/kali-root,dst=/root --mount src=kali-postgres,dst=/var/lib/postgresql kali zsh"' | tee -a ~/.bashrc
+# Grab the wrapper script so you can get into the container by just calling "kali"
+curl -s "https://raw.githubusercontent.com/linuxkd/kali-docker/master/kali" --output /tmp/kali
+chmod +x /tmp/kali
+sudo mv /tmp/kali /usr/local/bin
